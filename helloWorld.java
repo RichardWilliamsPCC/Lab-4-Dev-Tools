3a064e16736f36a27802f6d39bf4fe7e8e9f7fcd
Richard Williams <richard.williams12@pcc.edu>
2023-10-01 17:20:33 -0700

    Create helloWorld class

---
+++ helloWorld.java
@@ -0,0 +1,17 @@
+
+/**
+ * Say Hello!
+ *
+ * @author Richard Williams
+ * @version 2023.01.10
+ */
+public class helloWorld
+{
+    /**
+     * Say hello to everyone
+     */
+    public void sayHello()
+    {
+        System.out.println("Hello World!");
+    }
+}
