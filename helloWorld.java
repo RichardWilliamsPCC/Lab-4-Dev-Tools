42e5cc54a067c3d96336d40d86530441395a81a9
Richard Williams <richard.williams12@pcc.edu>
2023-10-23 10:28:30 -0700

    Print with name

--- helloWorld.java
+++ helloWorld.java
@@ -15,4 +15,12 @@ public class helloWorld
         System.out.println("Hello World!");
     }
     
+    /**
+     * Say hello to a specific person
+     * @param name
+     */
+    public void sayHello(String name)
+    {
+        System.out.println("Hello " + name + "!");
+    }
 }
